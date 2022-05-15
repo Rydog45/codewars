ee0b32ea7e8726bbf9b6b0425f89a52da33abf18
Rydog45 <102640282+Rydog45@users.noreply.github.com>
2022-05-15 03:08:57 -0700

    Update solution004.js

--- solution004.js
+++ solution004.js
@@ -2,7 +2,7 @@
 //You have an amount of money a0 > 0 and you deposit it with an interest rate of p percent divided by 360 per day on the 1st of January 2016. You want to have an amount a >= a0.
 //The function date_nb_days (or dateNbDays...) with parameters a0, a, p will return, as a string, the date on which you have just reached a. 
 function dateNbDays(a0, a, p) {
-    const startingDate = new Date('2022-01-03')
+    const startingDate = new Date('1-16-16')
     const dailyInterestRate = p / 36000
     let startingMoney = a0
     let daysPassed = 0
