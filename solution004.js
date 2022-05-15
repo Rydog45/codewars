67cde45c7355f3718b422b4fefdd8aa31d59ebe4
Rydog45 <102640282+Rydog45@users.noreply.github.com>
2022-05-15 03:14:09 -0700

    Update solution004.js

--- solution004.js
+++ solution004.js
@@ -3,7 +3,7 @@
 //The function date_nb_days (or dateNbDays...) with parameters a0, a, p will return, as a string, the date on which you have just reached a. 
 
 function dateNbDays(a0, a, p) {
-    const startingDate = new Date('1-16-16')
+    const startingDate = new Date('1-1-16')
     const dailyInterestRate = p / 36000
     let startingMoney = a0
     let daysPassed = 0
