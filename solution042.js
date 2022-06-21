b342e997a4c27c8393ac6251d92a4fc2feae47bc
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-06-21 14:33:37 -0700

    Add files via upload

---
+++ solution042.js
@@ -0,0 +1,15 @@
+// None of zese codevarriors seemz to remember hiz ovvn name !
+
+// Kould you help ?
+
+function Warrior(n){
+    let name = n;  
+    this.name = function(n){
+      if( n ) name=n;
+      return name;
+    }
+  }
+    
+  Warrior.prototype.toString = function(){
+      return "Hi! my name's "+this.name();
+  }
