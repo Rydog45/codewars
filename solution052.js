3fb95b749eeab0cb59ad2f7cbd854a456bccd22b
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-07-01 18:33:01 -0700

    Add files via upload

---
+++ solution052.js
@@ -0,0 +1,3 @@
+// You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+const truthy = [5, 15, 25, 35, 55];
+const falsy = [0, 0n, null, undefined, false, ];
