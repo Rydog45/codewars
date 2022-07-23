2ff9e692c5e26ee9f7a54f0c3026dd60fe67656a
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-07-22 22:29:19 -0700

    Add files via upload

---
+++ Solution070.js
@@ -0,0 +1,6 @@
+// When provided with a number between 0-9, return it in words.
+
+function switchItUp(number){
+    const words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    return words[number];
+  }
