85921a3733e3357ac2c7a1be10728dba23f5758c
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-08-03 21:57:29 -0700

    Add files via upload

---
+++ Solution081.js
@@ -0,0 +1,5 @@
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+function amIWilson(P) {
+    return P == 13 || P == 5 || P == 563 ? true : false
+  }
