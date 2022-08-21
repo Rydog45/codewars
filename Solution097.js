8855772234b97034187f18ef7c40df2705e2f589
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-08-20 22:08:58 -0700

    Add files via upload

---
+++ Solution097.js
@@ -0,0 +1,7 @@
+// In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+
+// For the purposes of this Kata, you may assume that all inputs are valid, i.e. arrays containing only integers.
+
+return arr.every(function (x, i) {
+    return i === 0 || x >= arr[i - 1];
+});
