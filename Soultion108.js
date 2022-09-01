2fe8ecbb62ba5175daeafc22889db2e2a315d242
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-08-31 18:40:32 -0700

    Add files via upload

---
+++ Soultion108.js
@@ -0,0 +1,8 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+function combineNames(First,last){
+    return First.concat(" ", last)
+    
+  
+    
+  }
