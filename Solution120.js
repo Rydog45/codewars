1c6fbdbe18cdad5f1472d96019ca2c264c3f2c38
Ryan Burks <102640282+Rydog45@users.noreply.github.com>
2022-09-12 23:14:32 -0700

    Add files via upload

---
+++ Solution120.js
@@ -0,0 +1,7 @@
+// In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+function withoutLast(arr) {
+    arr = [...arr]
+    arr.pop();
+    return arr;
+  }
